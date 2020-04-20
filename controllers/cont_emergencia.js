d77355caf0368c6689ea627a33d446b863ab794e
Felipe Medina <le17081434@merida.tecnm.mx>
2020-04-20 11:34:21 -0500

    Metodos creados
    Se agrego el metodo, buscar, eliminar, y actualizar/editar. Checar porfa

--- controllers/cont_emergencia.js
+++ controllers/cont_emergencia.js
@@ -5,9 +5,6 @@ const Op = db.Sequelize.Op;
 
 //Creacion de contactos de emergencia
 let create = (req, res) => {
-
-
-
     let cont_emergencia = {
         id: req.body.id,
         email: req.body.email,
@@ -19,7 +16,7 @@ let create = (req, res) => {
     //Guardando
     Cont_emergencia.create(cont_emergencia).then((data) => {
         res.status(200).send({
-            message: "Contacto de emergencia crado exitosamente"
+            message: "Contacto de emergencia creado exitosamente"
         })
     }).catch((err) => {
         res.status(400).send({
@@ -28,6 +25,73 @@ let create = (req, res) => {
     })
 
 };
+//Editar/actualizar
+let update = (req, res) => {
+    let id = req.body.id
+    Cont_emergencia.update(req.body, {
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Contacto de emergencia actualizado correctamente."
+                });
+            } else {
+                res.send({
+                    message: `No es posible actualizar el contacto de emergencia cin id=${id}. Es probable que el contacto no se haya encontrado o no existe`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error al acutualizar el contacto de emergencia con el id=" + id
+            });
+        });
+
+};
+//Encontrar 
+let findAll = (req, res) => {
+    let id = req.body.id
+    id = id ? {
+        id: {
+            [Op.like]: `%${id}%`
+        }
+    } : null;
+    Cont_emergencia.findAll({ where: id }).then((cont_emergencia) => {
+        res.send(cont_emergencia);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error ocurred while retrieving users"
+        })
+    })
+};
+//Borrar, por alguna razon me marcaba error cuando ponia delete, asi que lo deje en delet
+let delet = (req, res) => {
+    let id = req.body.id;
+
+    Cont_emergencia.destroy({
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Contacto eliminado correctamente"
+                });
+            } else {
+                res.send({
+                    message: `No se puede eliminar el contacto con id=${id}. No se encontro!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error al eliminar el contacto con id=" + id
+            });
+        });
+};
 module.exports = {
-    create
+    create,
+    update,
+    findAll,
+    delet
 }
--- routes/cont_emergencia.js
+++ routes/cont_emergencia.js
@@ -2,7 +2,10 @@ module.exports = app => {
     const cont_emergencia = require("../controllers/cont_emergencia")
     let router = require("express").Router()
 
-    router.post("/cont_emergencia", cont_emergencia.create)
+    router.post("/insert", cont_emergencia.create)
+    router.put("/update", cont_emergencia.update)
+    router.post("/find", cont_emergencia.findAll)
+    router.delete("/delete", cont_emergencia.delet)
 
     app.use('/vd', router)
 }
