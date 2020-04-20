5da624e0a0c2c48fbb31ec09b6ec2fbf4e556222
Juan Lopez <juan07alberto@gmai.com>
2020-04-20 13:51:53 -0500

    Protegiendo rutas

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,17 @@
 module.exports = app => {
     const user = require("../controllers/user")
+    const { isAuth } = require('../middlewares/auth')
 
     let router = require("express").Router()
 
     router.post("/user", user.create)
     router.post("/login", user.findAll)
     router.get("/", user.sendMsg)
+    router.get("/auth", isAuth, (req, res) => {
+        res.status(200).send({
+            message: "Es correcto"
+        })
+    })
     //put actualizar, delete eliminar, post insertar, get enviar datos
 
     app.use('/vd', router)
