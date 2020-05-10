ece1c3a67ee88b0b39fdd0154f7c68b5df584a44
Luis Tuyu <e17081482@itmerida.edu.mx>
2020-05-10 17:45:57 -0500

    agregando el modelo,duda con llave foranea

---
+++ models/convocatorias.js
@@ -0,0 +1,74 @@
+module.exports = (sequelize, Sequelize)=>{
+const convocatoria= sequelize.define("convocatoria",{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement:true,
+        allowNull: false,
+        validate: {
+            isNumeric: true
+        } 
+    },
+    id_empresa:{
+        type: Sequelize.INTEGER,
+        foreingkey: true //no estoy seguro de esto
+        ,allowNull:false
+        ,validate: {
+            isNumeric: true
+        }
+    },
+    titulo:{
+        type: Sequelize.STRING,
+        allowNull:false,
+        validate:{
+            notEmpety:true,
+        }
+    },
+    costo:{
+        type:Sequelize.FLOAT(9,2),
+        allowNull:false,
+        validate:{
+            isFLOAT:true
+        }
+    },
+    fecha:{
+        type: Sequelize.DATE,
+        allowNull: false,
+        validate:{
+            isDate: true
+        }
+    },
+    ref_modalidad:{
+        type:Sequelize.INTEGER,
+        notNUll: true,
+        validate:{
+        isNumeric:true
+    }
+    },
+    precio:{
+        type: Sequelize.FLOAT(9,2),
+        notNUll: true,
+        validate:{
+            isFLOAT:true
+        }
+
+    },
+    num_participantes:{
+        type: Sequelize.INTEGER,
+        notNUll: true,
+        validate:{
+            isNumeric:true
+        }
+    },
+    activate:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+
+    }
+
+},{
+    freezeTableNAme:true
+});
+return convocatoria;
+};
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize")
-const sequelize = new Sequelize("vd", "root", "", {
+const sequelize = new Sequelize("vd", "root", "123", {
     host: "localhost",
     dialect: "mysql",
     pool: {
@@ -17,6 +17,8 @@ db.sequelize = sequelize
 
 db.user = require("./user.js")(sequelize, Sequelize)
 db.empresa = require("./empresa.js")(sequelize, Sequelize)
-db.cont_emergencia = require("./cont_emergencia.js")(sequelize, Sequelize)
+db.cont_emergencia = require("./cont_emergencia.js")
+db.convocatorias = require("./convocatorias")
+(sequelize, Sequelize)
     //Agregar modelos
 module.exports = db
