19b2ad455b4a9a9dc3bb46be63d4c1ef2fee7d71
Luis Chi <chiojeda.luis@gmail.com>
2020-05-27 19:31:58 -0500

    eventos, modalidad

--- models/convocatorias.js
+++ models/convocatorias.js
@@ -1,5 +1,7 @@
 module.exports = (sequelize, Sequelize)=>{
-const convocatoria= sequelize.define("convocatoria",{
+const convocatoria = sequelize.define(
+  "convocatoria",
+  {
     /*id:{
         type:Sequelize.INTEGER,
         primaryKey: true,
@@ -9,73 +11,72 @@ const convocatoria= sequelize.define("convocatoria",{
             isNumeric: true
         } 
     },*/
-    id_empresa:{
-        type: Sequelize.STRING,
-        foreingkey: true //no estoy seguro de esto
-        ,allowNull:false
-        ,validate: {
-            isNumeric: true
-        }
+    id_empresa: {
+      type: Sequelize.STRING,
+      foreingkey: true, //no estoy seguro de esto
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
     },
-    titulo:{
-        type: Sequelize.STRING,
-        allowNull:false,
-        validate:{
-            notEmpty:true
-        }
+    titulo: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
-    banner:{
-        type: Sequelize.BLOB,
-        allowNull:false,
-        validate:{
-            notEmpty:true
-        }
+    banner: {
+      type: Sequelize.BLOB,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
-    costo:{
-        type:Sequelize.FLOAT(9,2),
-        allowNull:false,
-        validate:{
-            isFloat:true
-        }
+    costo: {
+      type: Sequelize.FLOAT(9, 2),
+      allowNull: false,
+      validate: {
+        isFloat: true,
+      },
     },
-    fecha:{
-        type: Sequelize.DATE,
-        allowNull: false,
-        validate:{
-            isDate: true
-        }
+    fecha: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      },
     },
-    ref_modalidad:{
-        type:Sequelize.INTEGER,
-        notNUll: true,
-        validate:{
-        isNumeric:true
-    }
+    ref_modalidad: {
+      type: Sequelize.TEXT,
+      notNUll: true,
+      validate: {
+        isNumeric: true,
+      },
     },
-    precio:{
-        type: Sequelize.FLOAT(9,2),
-        notNUll: true,
-        validate:{
-            isFloat:true
-        }
-
+    precio: {
+      type: Sequelize.FLOAT(9, 2),
+      notNUll: true,
+      validate: {
+        isFloat: true,
+      },
     },
-    num_participantes:{
-        type: Sequelize.INTEGER,
-        notNUll: true,
-        validate:{
-            isNumeric:true
-        }
+    num_participantes: {
+      type: Sequelize.INTEGER,
+      notNUll: true,
+      validate: {
+        isNumeric: true,
+      },
     },
-    active:{
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
-
-    }
-
-},{
-    freezeTableNAme:true
-});
+    active: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    freezeTableNAme: true,
+  }
+);
 return convocatoria;
 };
